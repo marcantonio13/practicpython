b50dba26468060b82a269f0c2caac62e94b4576e
marcantonio13 <antonio.avellaneda13@gmail.com>
2026-01-08 10:49:55 -0600

    Gestor de clientes

--- ProyectoFinal/tests/test_database.py
+++ ProyectoFinal/tests/test_database.py
@@ -1,6 +1,9 @@
 import copy
+import csv
+import config
 import unittest
 import database as db
+import helpers
 
 
 class TestDatabase(unittest.TestCase):
@@ -36,6 +39,30 @@ class TestDatabase(unittest.TestCase):
         cliente_borrado = db.Clientes.borrar('48H')
         cliente_rebuscado = db.Clientes.buscar('48H')
         self.assertNotEqual(cliente_borrado, cliente_rebuscado)
+
+
+    def test_dni_valido(self):
+
+        self.assertTrue(helpers.dni_valido('00A', db.Clientes.lista))
+        self.assertFalse(helpers.dni_valido("232323S", db.Clientes.lista))
+        self.assertFalse(helpers.dni_valido("F35", db.Clientes.lista))
+        self.assertFalse(helpers.dni_valido("48H", db.Clientes.lista))
+
+
+
+    def test_escritura_csv(self):
+        db.Clientes.borrar('48H')
+        db.Clientes.borrar('15J')
+        db.Clientes.modificar('28Z','Mariana', 'Garcia')
+
+        dni, nombre , apellido = None, None, None
+        with open(config.DATABASE_PATH, newline='\n') as fichero:
+            reader = csv.reader(fichero, delimiter=';')
+            dni, nombre, apellido = next(reader)
+
+        self.assertEqual(dni, '28Z')
+        self.assertEqual(nombre, 'Mariana')
+        self.assertEqual(apellido, 'GArcia')
 
 
 if __name__ == '__main__':
